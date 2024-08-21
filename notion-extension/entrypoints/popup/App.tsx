d2de496b3de7117431dec930181e49c68e0fcf98
Amo Taiki <c1101081@planet.kanazawa-it.ac.jp>
2024-08-22 04:00:48 +0900

    いったん

--- notion-extension/entrypoints/popup/App.tsx
+++ notion-extension/entrypoints/popup/App.tsx
@@ -1,33 +1,100 @@
-import { useState } from 'react';
+/* import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
-import './App.css';
+import wxtLogo from '/wxt.svg'; */
+import {useForm} from "@conform-to/react"
+import { parseWithValibot } from "conform-to-valibot";
+import {object,string,date} from "valibot"
+import "./style.css"
+import { useStopwatch } from 'react-timer-hook';
+import { storage } from "wxt/storage";
+
+const schema = object({
+  title: string("Title is Required"),
+})
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  const {
+    /* totalSeconds, */
+    seconds,
+    minutes,
+    hours,
+    /* days,
+    isRunning, */
+    start,
+    pause,
+    /* reset, */
+  } = useStopwatch();
+
+  const [form,{title}] = useForm({
+    id: "notion-submit",
+    defaultValue: {
+      title: "Tested"
+    },
+    onValidate({formData}) {
+      return parseWithValibot(formData,{
+        schema
+      })
+    },
+    async onSubmit(event, context) {
+      event.preventDefault();
+      const formData = await context.formData
+      const submission = await parseWithValibot(formData, {
+        schema,
+      });
+
+      // Send the submission back to the client if the status is not successful
+      if (submission.status !== 'success') {
+        return submission.reply();
+      }
+      
+    }})
+
+    async function TimeStrage(){
+      start()
+      const SaveTime = await storage.getItem<string>("local:timer")
+      if(SaveTime === null){
+        const time = `${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`
+        await storage.setItem("local:timer",time)
+        document.getElementById("now")!.textContent = time
+      }
+      document.getElementById("now")!.textContent = SaveTime
+    } 
 
   return (
     <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
+    <main className="p-5">
+      <h1 className="text-2xl border-black rounded-full bg-slate-200 text-center text-bold p-3">活動記録</h1>
+      <div className="text-lg text-center" id="now">Please Timer Start</div>
+      <div className="flex flex-auto"> 
+          {[TimeStrage,pause].map((val,idx) => <button 
+            className="border-black rounded-xl hover:bg-slate-300 bg-slate-200 py-2 px-3 mx-2"
+            onClick={val}
+          >
+            {idx === 0 ? "start" : "stop"}
+          </button>)}
+        </div>
+      <form 
+        className="flex flex-col" 
+        id={form.id} 
+        onSubmit={form.onSubmit}
+      >
+        <div className="flex flex-col">
+          <label>Title</label>
+          <input 
+            className="border-black border rounded-md px-1" 
+            type="text" 
+            name="title"
+          />
+          <div>{title.errors}</div>
+        </div>
+        <button 
+          className="hover:bg-blue-300 border-black justify-center items-center border bg-blue-100 rounded-2xl py-[2.5px] px-3 mt-3 text-center" 
+          form={form.id}>
+            Submit
+          </button>
+      </form>
+    </main>
     </>
   );
 }
---
+++ notion-extension/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./entrypoints/**/*.{tsx,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
